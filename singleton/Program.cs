566da143797a51a34e1e59f35de8160a415da1d3
Waldemar Muhl <waldemarmuhl@gmail.com>
2024-03-23 12:45:03 +0000

    added singleton project

---
+++ singleton/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace singleton
+{
+    // The 'Singleton' class
+    internal class Singleton
+    {
+        // A private static variable that holds the single instance of the Singleton
+        private static Singleton? _instance = null;
+
+        // A private constructor to prevent instantiation outside the class
+        private Singleton()
+        {
+        }
+
+        // The public static method that controls access to the Singleton instance
+        public static Singleton Instance
+        {
+            get
+            {
+                // If the instance is null, create a new one
+                if (_instance == null)
+                {
+                    _instance = new Singleton();
+                }
+
+                // Return the Singleton instance
+                return _instance;
+            }
+        }
+
+        // An example method that the Singleton class might provide
+        public void DoSomething()
+        {
+            Console.WriteLine("Doing something...");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Trying to create instances of the Singleton
+            Singleton firstInstance = Singleton.Instance;
+            Singleton secondInstance = Singleton.Instance;
+
+            // Demonstrating that both variables hold the same instance
+            if (firstInstance == secondInstance)
+            {
+                Console.WriteLine("Both instances are the same.");
+            }
+
+            // Using the Singleton instance to perform an action
+            firstInstance.DoSomething();
+        }
+    }
+}
---
+++ singleton/README.md
@@ -0,0 +1,11 @@
+# Singleton Design Pattern
+## Description
+A simple implementation of a singleton in C#.  Please note that this is not a thread-safe example, please see the thread safe example instead.
+
+## Setup Instructions 
+
+1. Clone the repo
+2. `dotnet run`
+
+## Further Reading
+For more information see [https://www.waldemarmuhl.com/Posts/singleton-pattern](https://www.waldemarmuhl.com/Posts/singleton-pattern)
